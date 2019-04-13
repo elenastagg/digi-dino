353c7ff4ff28b2703f9f4f9ef944e232a2dea455
Amy Hart <xamyhart@gmail.com>
2019-04-13 13:10:12 +0100

    Updating names of Animations and Button components

--- src/components/home.js
+++ src/components/home.js
@@ -1,77 +1,68 @@
 import React, { Component } from 'react';
-import {
-  TextInput,
-  View,
-  StyleSheet,
-  Text,
-} from 'react-native';
-import Button from './button';
+import { TextInput, View, StyleSheet, Text } from 'react-native';
+import Button from './Button';
 import colors from './colors';
 
 const style = StyleSheet.create({
-  view: {
-    width: '100%',
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: colors.darkgreen,
-  },
-  input: {
-    color: colors.white,
-    fontSize: 30,
-    margin: 10,
-    fontFamily: 'Chewy',
-    textAlign: 'center',
-  },
-  buttonBackground: {
-    backgroundColor: colors.midgreen,
-    width: 90,
-    marginLeft: 10,
-  },
-  buttonText: {
-    color: 'white',
-    fontSize: 30,
-    fontFamily: 'Chewy',
-  },
+	view: {
+		width: '100%',
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: colors.darkgreen
+	},
+	input: {
+		color: colors.white,
+		fontSize: 30,
+		margin: 10,
+		fontFamily: 'Chewy',
+		textAlign: 'center'
+	},
+	buttonBackground: {
+		backgroundColor: colors.midgreen,
+		width: 90,
+		marginLeft: 10
+	},
+	buttonText: {
+		color: 'white',
+		fontSize: 30,
+		fontFamily: 'Chewy'
+	}
 });
 
 class DinoName extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { text: '' };
-  }
+	constructor(props) {
+		super(props);
+		this.state = { text: '' };
+	}
 
-  handleTextChange(text) {
-    this.setState({
-      text: text,
-    });
-  }
+	handleTextChange(text) {
+		this.setState({
+			text: text
+		});
+	}
 
-  render() {
-    return (
-      <View style={style.view}>
-        <Text style={style.input}>
-          Welcome to Digi Dino!
-        </Text>
-        <Text style={style.input}>
-          Please give your dinosaur a name below
-        </Text>
-        <TextInput
-          style={style.input}
-          placeholder="TYPE NAME HERE"
-          placeholderTextColor={colors.midgreen}
-          onChangeText={text => this.handleTextChange(text)}
-          value={this.state.text}
-        />
-        <Button
-          onPress={() => this.props.onSubmit(this.state.text)}
-          title="Save"
-          backgroundStyle={style.buttonBackground}
-          textStyle={style.buttonText}
-        />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={style.view}>
+				<Text style={style.input}>Welcome to Digi Dino!</Text>
+				<Text style={style.input}>Please give your dinosaur a name below</Text>
+				<TextInput
+					style={style.input}
+					placeholder="TYPE NAME HERE"
+					placeholderTextColor={colors.midgreen}
+					onChangeText={(text) => this.handleTextChange(text)}
+					value={this.state.text}
+				/>
+				<Button
+					onPress={() => this.props.onSubmit(this.state.text)}
+					title="Save"
+					backgroundStyle={style.buttonBackground}
+					textStyle={style.buttonText}
+				/>
+			</View>
+		);
+	}
 }
 
 export default DinoName;
--- src/components/pet-screen.js
+++ src/components/pet-screen.js
@@ -1,44 +1,39 @@
 import React, { Component } from 'react';
 import { Dimensions, View, StyleSheet } from 'react-native';
-import Animation from './animation';
+import Animation from './Animation';
 
 // Get the width of the device's screen
 // Set the width of PetScreen to 80% of the devices screen
 // Set the height of the PetScreen, equal to the width of the PetScreen
 const { width } = Dimensions.get('window');
-const gameScreenWidth = (width / 100) * 80;
+const gameScreenWidth = width / 100 * 80;
 
 // StyleSheet
 const styles = StyleSheet.create({
-  petScreen: {
-    overflow: 'hidden',
-    height: gameScreenWidth,
-    width: gameScreenWidth,
-    alignSelf: 'center',
-  },
+	petScreen: {
+		overflow: 'hidden',
+		height: gameScreenWidth,
+		width: gameScreenWidth,
+		alignSelf: 'center'
+	}
 });
 
 // PetScreen component
 class PetScreen extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-     
-    };
-  }
+	constructor(props) {
+		super(props);
+		this.state = {};
+	}
 
-  render() {
-    return (
-      <View style={styles.petScreen}>
-        <Animation
-          animationName={this.props.currentAction}
-        />
-      </View>
-    );
-  }
+	render() {
+		return (
+			<View style={styles.petScreen}>
+				<Animation animationName={this.props.currentAction} />
+			</View>
+		);
+	}
 
-  componentDidUpdate() {
-  }
+	componentDidUpdate() {}
 }
 
 export default PetScreen;
